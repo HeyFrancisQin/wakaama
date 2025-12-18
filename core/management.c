982cd52a707aa002c32c05529e7d8f499b9d610f
HeyFrancisQin <1371316129@qq.com>
2025-12-18 09:45:16 +0800

    修改默认打包格式为SENML CBOR，解决资源实例读取失败的问题

--- core/management.c
+++ core/management.c
@@ -178,7 +178,7 @@ uint8_t dm_handleRequest(lwm2m_context_t * contextP,
     }
     else
     {
-        format = LWM2M_CONTENT_TLV;
+        format = LWM2M_CONTENT_SENML_CBOR;
     }
 
     if (uriP->objectId == LWM2M_SECURITY_OBJECT_ID)
